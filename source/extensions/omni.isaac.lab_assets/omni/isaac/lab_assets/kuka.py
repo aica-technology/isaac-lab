35f7607b41853aba671cd58c53359d51eab01027
Yehya El Hassan <yehya@aica.tech>
2024-12-18 15:34:35 +0200

    fix: remove comments

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/kuka.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/kuka.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.actuators import ImplicitActuatorCfg
 from omni.isaac.lab.assets.articulation import ArticulationCfg
