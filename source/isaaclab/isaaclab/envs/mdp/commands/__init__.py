fa51af132f92731afb5c75ce5cb3d06511237ae0
Yehya El Hassan <yehya@aica.tech>
2025-04-22 14:24:34 +0300

    fix: remove un-related files

--- source/isaaclab/isaaclab/envs/mdp/commands/__init__.py
+++ source/isaaclab/isaaclab/envs/mdp/commands/__init__.py
@@ -11,14 +11,10 @@ from .commands_cfg import (
     TerrainBasedPose2dCommandCfg,
     UniformPose2dCommandCfg,
     UniformPoseCommandCfg,
-    UniformVelocityCommandCfg,
-    UniformObjectLocationCfg,
-    UniformPoseVelocityCommandCfg
+    UniformVelocityCommandCfg
 )
 from .null_command import NullCommand
 from .pose_2d_command import TerrainBasedPose2dCommand, UniformPose2dCommand
 from .pose_command import UniformPoseCommand
 from .velocity_command import NormalVelocityCommand, UniformVelocityCommand
-from .object_command import UniformObjectLocationCommand
-from .pose_velocity_command import UniformPoseVelocityCommand
 
