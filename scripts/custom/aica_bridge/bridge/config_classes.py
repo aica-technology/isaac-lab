07b64b92bac0cc75dd18fe0a661932e0d924c0f7
Yehya El Hassan <yehya@aica.tech>
2025-06-06 16:23:12 +0300

    fix: remove simulation parameters

--- scripts/custom/aica_bridge/bridge/config_classes.py
+++ scripts/custom/aica_bridge/bridge/config_classes.py
@@ -8,9 +8,3 @@ class BridgeConfig:
     command_port: int = 1802
     force_port: int = 1803
     force_sensor_name: str = None
-
-
-@dataclass
-class SimulationParameters:
-    device: str = "cuda:0"
-    dt: float = 1 / 60
