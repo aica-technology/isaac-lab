24e74b86fdfa74f08754550eb7e1fb652f854682
Yehya El Hassan <yehya@aica.tech>
2025-06-26 10:51:40 +0300

    feat: develop impedance rl scene

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/control/config/ur5e/__init__.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Velocity-Impedance-Control-UR5e-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.velocity_impedance_cfg:UR5eVelocityImpedanceControlSceneCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UR5eVelocityImpedancePPORunnerCfg",
+    },
+)
