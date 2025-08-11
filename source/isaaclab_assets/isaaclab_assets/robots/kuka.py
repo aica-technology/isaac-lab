c9d0b37a4dff706a7a16eca199c4d9556ba8c461
Yehya El Hassan <yehya@aica.tech>
2025-08-11 10:38:08 +0200

    fix: create effort limit constants

--- source/isaaclab_assets/isaaclab_assets/robots/kuka.py
+++ source/isaaclab_assets/isaaclab_assets/robots/kuka.py
@@ -2,6 +2,14 @@ import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg, VelocityPIDActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 
+KR210_EFFORT_LIMITS = {
+    "kr210_joint_a1": 4100,
+    "kr210_joint_a2": 4100,
+    "kr210_joint_a3": 4100,
+    "kr210_joint_a4": 1250,
+    "kr210_joint_a5": 1250,
+    "kr210_joint_a6": 700,
+}
 
 KUKA_KR210_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
@@ -41,28 +49,15 @@ KUKA_VEL_KR210_CFG.actuators["arm"].stiffness = 0
 KUKA_VEL_KR210_CFG.actuators["arm"].damping = 50000
 
 KUKA_KR210_LOW_LEVEL_PID_CFG = KUKA_KR210_CFG.copy()
+
 KUKA_KR210_LOW_LEVEL_PID_CFG.actuators = {
     "arm": VelocityPIDActuatorCfg(
             joint_names_expr=[".*"],
-            effort_limit={
-                "kr210_joint_a1": 4100,
-                "kr210_joint_a2": 4100,
-                "kr210_joint_a3": 4100,
-                "kr210_joint_a4": 1250,
-                "kr210_joint_a5": 1250,
-                "kr210_joint_a6": 700,
-            },
+            effort_limit=KR210_EFFORT_LIMITS,
             proportional_gain=400,
             derivative_gain=0,
             integral_gain=20000,
-            max_integral_error={
-                "kr210_joint_a1": 4100,
-                "kr210_joint_a2": 4100,
-                "kr210_joint_a3": 4100,
-                "kr210_joint_a4": 1250,
-                "kr210_joint_a5": 1250,
-                "kr210_joint_a6": 700,
-            },
+            max_integral_error=KR210_EFFORT_LIMITS,
             delta_time=0.002
         ),
     }
