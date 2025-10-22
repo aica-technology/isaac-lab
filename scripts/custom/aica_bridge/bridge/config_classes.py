cacbf186ed12982fa6d16a6345e69ee5b5e749bd
Yehya El Hassan <yehya@aica.tech>
2025-10-22 09:27:15 +0200

    fix: simplify simulator logic

--- scripts/custom/aica_bridge/bridge/config_classes.py
+++ scripts/custom/aica_bridge/bridge/config_classes.py
@@ -6,5 +6,5 @@ class BridgeConfig:
     address: str = "*"
     state_port: int = 1801
     command_port: int = 1802
-    force_port: int = 1803
-    force_sensor_name: str = None
+    ft_sensor_port: int = 1803
+    ft_sensor_name: str = None
